5edbfaef238ccca2b64ca5cc0c57dded9a296f1a
Bryan <artificialknowledge217@gmail.com>
2017-05-02 12:11:06 -0400

    add previous guesses container

--- src/components/GamePlay.js
+++ src/components/GamePlay.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Result from './Result';
 import GuessField from './GuessField';
 import GuessNumber from './GuessNumber';
+import PreviousGuesses from './PreviousGuesses'
 
 export default class GamePlay extends React.Component {
 	constructor(props){
@@ -26,23 +27,36 @@ export default class GamePlay extends React.Component {
 		let range = randomNumber - this.state.guess; 
 		
 		if ( range < 11 && range > -11 ) {
-			return this.setState({ hotOrCold: 'You Are Burning Hot!', guessNum: this.state.guessNum + 1 })
+			return this.setState({ 
+				hotOrCold: 'You Are Burning Hot!', 
+				guessNum: this.state.guessNum + 1, 
+				previousGuesses: this.state.previousGuesses.concat([this.state.guess]) 
+			})
 		}
 
 		if ( this.state.guess === randomNumber ) {
-			return this.setState({ hotOrCold: 'You Are Correct!', guessNum: this.state.guessNum + 1 })
+			return this.setState({ 
+				hotOrCold: 'You Are Correct!', 
+				guessNum: this.state.guessNum + 1,
+				previousGuesses: this.state.previousGuesses.concat([this.state.guess])
+			})
 		}
 
-		return this.setState({ hotOrCold: 'You Are Freezing Cold!', guessNum: this.state.guessNum + 1 })
+		return this.setState({ 
+			hotOrCold: 'You Are Freezing Cold!', 
+			guessNum: this.state.guessNum + 1,
+			previousGuesses: this.state.previousGuesses.concat([this.state.guess]) 
+		})
 	}
 
 	render() {
 		return (
-			<div>
+			<div className="container">
 				<Result result={this.state.hotOrCold} />
 				<GuessField onClick={this.submitGuess} 
 				onChange={this.handleChange} />
 				<GuessNumber number={this.state.guessNum} />
+				<PreviousGuesses previousGuesses={this.state.previousGuesses} />
 			</div>
 		)
 	}
---
+++ src/components/PreviousGuesses.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function PreviousGuesses (props) {
+	let prevGuesses = props.previousGuesses;
+	let prevGuessList = prevGuesses.map((guess, index) => {
+		return (<li className="guess-list-item" key={index}>
+					{guess}
+				</li>
+		);
+	})
+	return (
+		<div className="previous-guesses">
+			<ul className="guess-list">{prevGuessList}</ul>
+		</div>
+	)
+}
